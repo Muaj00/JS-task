80257b3f4d5162b3cf8a79cfbfe2b2b367dafbab
Md. Muaj Chowdhury <96824128+Muaj00@users.noreply.github.com>
2022-04-22 21:02:53 +0600

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -34,6 +34,388 @@ console.log(index);
  email = 'sadiasnfdf@gmkf';
  let res = email.replace('k','w');
  console.log(res);
+
+/part 3
+const naes = ['shaun','mario','luigi'];
+for(let i=0;i<naes.length;i++){
+    //console.log(names[i]);
+    let html = `<div>${naes[i]}</div>`;
+    console.log(html);
+}
+ 
+ 
+//while loop
+ 
+const names=['shaun','mario','luigi'];
+ 
+let i=0;
+while(i<5){
+   console.log('in loop',i);
+      i++;
+}
+let i=0;
+while(i<names.length){
+    console.log(names[i]);
+    i++;
+}
+ 
+//do while loop
+ 
+let i=4;
+do{
+    console.log('value of i is ',i);
+    i++;
+}while(i<5);
+ 
+//if statement
+ 
+const age=25;
+if(age>20){
+    console.log('you are over 20 yerars of age');
+}
+const ninjas=['saymon','ikram'.'muaj','sinthia'];
+if (ninjas.length>3){
+    console.log('that\'s a lot of ninjas');
+}
+const password='password12345';
+if(password.length>=12){
+    console.log('that password is mighty strong');
+}else if(password.length>=8){
+    console.log('that password is long enough');
+}else{
+    console.log('password is not long enough')
+}
+ 
+ 
+ 
+//logical operators -OR || AND &&
+const password='p@ssword12345';
+if(password.length>=12 && password.includes('@')){
+    console.log('that password is mighty strong');
+}else if(password.length>=8 || password.includes('@') && password.length>=5){
+    console.log('that password is strong enough');
+}else{
+    console.log('password is not strong enough')
+}
+ 
+ 
+//logical NOT(!)
+let user = false;
+if(!user){
+    console.log('you must be logged in to continue');
+}
+console.log(!true);
+console.log(!false);
+ 
+ 
+//break and continue
+const scores = [50,25,0,30,100,20,10];
+for(let i=0;i<scores.length;i++){
+ 
+    if(scores[i]===0){
+        continue;
+    }
+ 
+    console.log('your score: ',scores[i]);
+    if(scores[i]===100){
+        console.log('congrates!you got the top score.');
+        break;
+    }
+}
+ 
+ 
+//switch statements
+ 
+const grade='D';
+switch(grade){
+    case 'A':
+        console.log('You got an A!');
+        break;
+    case 'B':
+        console.log('You got a B!');
+                break;
+    case 'C':
+        console.log('You got a C!');
+        break;
+    case 'D':
+        console.log('You got a D!');
+        break;
+    case 'E':
+        console.log('You got an E!');
+        break;
+    default:
+        console.log('Not a valid grade.');
+}
+ 
+ 
+ 
+//variable &block scope
+let age=30;
+let name='Sam';
+if(true){
+    age=40;
+    let name='Shaun';
+    console.log('inside 1st code block: ',age,name);
+    if(true){
+        let age=50;
+        console.log('inside the 2nd block: ',age);
+    }
+}
+console.log('outside 1st code block: ',age,name);
+*/
+
+
+/*
+//part 4 
+/function declaration
+function greet() {
+    console.log('helo there')
+}
+greet();
+// function expressions
+const speak = function() {
+    console.log('good day!')
+};
+speak();
+//arguments and parameters
+const speak = function(name) {
+    console.log('good day ${name}')
+};
+speak('mario')
+// passing different parameter/arguments
+const speak = function(name, time) {
+    console.log('good ${time} ${name}')
+};
+speak('mario', 'morning');
+// give this parameters some different values
+const speak = function(name = 'luigi', time = 'night') {
+    console.log('good ${time} ${name}')
+};
+speak();
+//returning values
+const calcArea = function(radius) {
+    return 3.14 * radius ** 2;
+};
+const area = calcArea(5);
+console.log(area);
+//regular function
+const calcArea = function(radius) {
+    return 3.14 * radius ** 2;
+};
+//arrow function
+const calcarea = radius => 3.14 * radius ** 2;
+//two parameters we have to use parenthesis
+const area = calcArea(5);
+console.log('area is :', area);
+//practise arrow function
+const greet = function() {
+    return 'hello,world';
+};
+const greet = () => 'hello,world';
+const result = greet();
+console.log(result);
+// practise arrow function 
+const bill = function(products, taxs) {
+    let total = 0;
+    for (let i = 0; i < products.length; i++) {
+        total += products[i] + products[i] * taxs;
+    }
+    return total;
+}
+const bill = (products, taxs) => {
+    let total = 0;
+    for (let i = 0; i < products.length; i++) {
+        total += products[i] + products[i] * taxs;
+    }
+    return total;
+};
+console.log(bill([10, 15, 30], 0.2));
+//method and function 
+const name = 'shaun'
+//function 
+const greet = () => 'hello';
+let resultOne = greet();
+console.log(resultOne);
+// method
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
+// callbacks & foreach
+const myFunc = (callbackFunc) => {
+    //do something
+    let value = 50;
+    callbackFunc(value);
+};
+myFunc(value => {
+    //do something
+    console.log(value);
+});
+// foreach
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+people.forEach(person => {
+    console.log(person)
+});
+//using index in second argument in arrow function
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+people.forEach(person, index) => {
+console.log(index, person)
+});
+// callbacks and foreach together 
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+const logPerson = (person, index) => {
+    console.log('${index} - hello $ {person}');
+};
+people.forEach(logPerson);
+// get a reference to the 'ul'
+const ul = document.querySelector('.people');
+const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+let html = ' ';
+people.forEach(person => {
+    // create html template
+    html += '<li style="color: purple">${person}</li>';
+});
+console.log(html);
+ul.innerHTML = html;
+//part5
+// const blogs=[
+//     {title: 'why mac & cheese rules', likes:30},
+//     {title: '10 things to make with marmite',likes:50}
+// ],
+// console.log(blogs);
+let user={
+    name: 'crysta',
+    age: 30,
+    email: 'crystal@gmail.com',
+    location: 'berlin',
+    //blogs: ['why mac & cheese rules','10 things to make with marmite'],
+    blogs=[
+        {title: 'why mac & cheese rules', likes:30},
+        {title: '10 things to make with marmite',likes:50}
+    ],
+    login: function(){
+        console.log('the user logged in');
+    },
+    logout: function(){
+        console.log('the user loged out');
+    },
+    
+    logBlogs: function(){
+        //console.log(this.blogs);
+        console.log('this user has written the following blogs:');
+        this.blogs.forEach(blog => {
+            console.log(blog.title,blog.likes);
+        })
+    }
+};
+// console.log(user);
+// console.log(user.name);
+// //user.age=35
+// console.log(user.age);
+// const key = 'location';
+// console.group(user[key]); user['location']
+// user['name']='chun-li';
+// console.log(user['name']);
+// console.log(typeof user);
+//--------------------------------------------------------
+// user.login();
+// user.logout();
+// const name = 'mario';
+// name.toUpperCase();
+//--------------------------------------------------------
+user.logBlogs();
+console.log(this);
+//math object
+console.log(Math);
+console.log(Math.Pi);
+console.log(Math.E);
+const area= 7.7;
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.cell(area));
+console.log(Math.trunc(area));
+//random numbers
+const random =Math.random();
+console.log(random);
+console.log(Math.round(random*100));
+//primitive values
+let scoreOne=50;
+let scoreTwo = scoreOne;
+console.log('scoreOne:${scoreOne}','scoreTwo: $(scoreTwo}');
+scoreOne = 100;
+console.log('scoreOne: ${scoreOne}','scoreTwo: ${scoreTwo}');
+//refernce values
+const userOne ={ name:'ryu',age:30};
+const userTwo= userOne;
+console.log(userOne, usertwo);
+userOne.age =40;
+console.log(userOne, userTwo);
+//part 6
+// get an element by ID
+//const tittle = document.getElementById('para-tittle');
+//console.log(tittle);
+// get elements by their class name
+//const errors = document.getElementsByclassNmae('error');
+//console.log(errors)
+//const para = document.querySeclector('body > h1');
+//console.log(para);
+// get element by their tag name
+//const paras = document.getElementsByTagName('p');
+//console.log(paras);
+//console.log(paras[1]);
+//const para = document.querySelector('p');
+//consle.log(para.innerText);
+// para.innerText ='ninjas are wesome'
+//const paras = document.querySelectorAll('p');
+//paras.forEach(para =>){
+   // console.log(para.innerText);
+   // para.innerText += 'new text';
+//}}
+//const content = document.querySelector('.content');
+//console.log(content.innerHTML);
+//content.innerHTML += '>h2< THIS IS A NEW HTML H2</h2>'
+//const people = ['hulu','lungi','jamai'];
+//people.forEach(person =>){
+    //content.innerHTML +='<p>${person}</p>';
+//}};
+//const content = document.querySelector('p');
+//parseFloat.forEach()
+//console.log(content.classlist);
+//const link = document.querySelector('a');
+//console.log(link.getAttribute('href'));
+//link.setAttribute('href','http://www.thenetninja.co.uk');
+//const mssg = document.querySelector('p');
+//console.log(mssg.getAttribute('class'));
+//mssg.setAttribute('class','success');
+//mssg.setAttribute('style','color: green;');
+//const tittle = document.querySelector('h1');
+//tittle.setAttribute('style','margin : 50px');
+//console.log(tittle.style);
+//console.log(tittle.style.color);
+//tittle.style.margin = '50px';
+//tittle.style.color = 'crimsom';
+//tittle.style.frontSize ='60px';
+//tittle.style.margin = '';
+//const content = document.querySelector('p');
+//console.log(content.classList);
+//content.classList.add('error');
+//content.classList.remove('error');
+//content.classList.add('success');
+const paras = document.querySelectorAll('p');
+paras.forEach(p=> {
+  //console.log(p.innerText);
+  if(p.textContent.includes('error')){
+      p.classList.add('error');
+  }
+  if(p.innerText.includes('success')){
+      p.classList.add('success');
+  }
+ } );
+ const tittle =document.querySelector('.tittle');
+ 
+ tittle.classList.toggle('test');
+ tittle.classList.toggle('test');
+*
+
+
 let radius = 10;
 const pi = 3.14;
 console.log(radius, pi);
